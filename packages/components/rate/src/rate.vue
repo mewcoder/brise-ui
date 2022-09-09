3925f05592b7841d541b6aadce8de8ee1139b496
mewcoder <1010953107@qq.com>
2022-09-09 22:27:46 +0800

    feat: update rate

--- packages/components/rate/src/rate.vue
+++ packages/components/rate/src/rate.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="classObj">
+  <div class="a-rate" :class="{ 'is-disabled': disabled }">
     <span
       v-for="(item, index) in 5"
       :key="index"
@@ -9,7 +9,16 @@
       @mouseleave="resetCurrentValue"
       @click="selectValue(item)"
     >
-      &#9733;
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 576 512"
+      >
+        <path
+          d="M259.3 17.8L194 150.2L47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103l-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5l105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2L316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"
+          fill="currentColor"
+        ></path>
+      </svg>
     </span>
   </div>
 </template>
@@ -20,11 +29,6 @@ import { rateProps } from './rate';
 
 const props = defineProps(rateProps);
 const emit = defineEmits(['update:modelValue', 'change']);
-
-const classObj = computed(() => ({
-  'a-rate': true,
-  'a-rate--disabled': props.disabled
-}));
 
 let currentValue = ref(props.modelValue);
 
