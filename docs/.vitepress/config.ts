668d2460bfbc9b33c89d2c53b9422d354bd16af2
mewcoder <1010953107@qq.com>
2022-09-10 20:31:51 +0800

    docs: udpate

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,11 +1,11 @@
 export default {
-  title: 'atomu',
+  title: 'Atomu',
   description: 'Vue3 Component Library',
   appearance: false,
   head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
   themeConfig: {
     logo: '/atom.svg',
-    siteTitle: 'atomu',
+    siteTitle: 'Atomu',
     docsDir: 'docs',
     outlineTitle: '#',
     repo: 'https://github.com/mewcoder/atomu',
@@ -19,14 +19,14 @@ export default {
           items: [{ text: '🚀 快速开始', link: '/guide/start' }]
         },
         {
-          text: '基础组件',
+          text: '基础组件(2)',
           items: [
             { text: 'Icon 图标', link: '/component/icon' },
             { text: 'Button 按钮', link: '/component/button' }
           ]
         },
         {
-          text: '表单组件',
+          text: '表单组件(8)',
           items: [
             { text: 'Input 输入框', link: '/component/input' },
             { text: 'Radio 单选框', link: '/component/radio' },
@@ -34,11 +34,12 @@ export default {
             { text: '🚧 Select 下拉框 ', link: '/component/select' },
             { text: 'Switch 开关 ', link: '/component/switch' },
             { text: 'Rate 评分', link: '/component/rate' },
+            { text: '🚧 DatePicker 日期选择期', link: '/component/datePicker' },
             { text: 'Form 表单 ', link: '/component/form' }
           ]
         },
         {
-          text: '数据展示组件',
+          text: '数据展示组件(4)',
           items: [
             { text: '🚧 Table 表格 ', link: '/component/table' },
             { text: '🚧 Tree 树 ', link: '/component/tree' },
@@ -47,10 +48,22 @@ export default {
           ]
         },
         {
-          text: '反馈组件',
+          text: '导航(4)',
           items: [
+            { text: '🚧 Menu 菜单 ', link: '/component/menu' },
+            { text: '🚧 Breadcrumb 面包屑 ', link: '/component/breadcrumb' },
+            { text: '🚧 Tabs 标签页', link: '/component/tabs' },
+            { text: '🚧 Backtop 回到顶部 ', link: '/component/backtop' }
+          ]
+        },
+        {
+          text: '反馈组件(5)',
+          items: [
+            { text: '🚧 Alert 提示 ', link: '/component/alert' },
             { text: '🚧 Message 信息 ', link: '/component/message' },
-            { text: 'Dialog 对话框 ', link: '/component/dialog' }
+            { text: '🚧 Notification 通知 ', link: '/component/notification' },
+            { text: 'Dialog 对话框 ', link: '/component/dialog' },
+            { text: 'Drawer 抽屉 ', link: '/component/drawer' }
           ]
         }
       ]
