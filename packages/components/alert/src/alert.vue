636cfb700b08010e6209fd5254bf39ed3e9c5ea4
mewcoder <1010953107@qq.com>
2022-09-12 00:33:53 +0800

    feat: delete icon

--- packages/components/alert/src/alert.vue
+++ packages/components/alert/src/alert.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="alert-fade">
     <div v-if="visible" :class="classObj">
-      <CheckCircleIcon />
+      <CheckCircleIcon class="w-5 h-5"/>
       <slot>
         <p class="a-alert__content">{{ title }}</p>
       </slot>
@@ -13,7 +13,7 @@
 import { computed, ref } from 'vue';
 import { alertProps } from './alert';
 import { AIconClose } from '../../inner';
-import { CheckCircleIcon } from 'atomu-heroicons';
+import { CheckCircleIcon } from '@heroicons/vue/20/solid';
 
 const props = defineProps(alertProps);
 
